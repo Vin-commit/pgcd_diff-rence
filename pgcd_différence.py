4a59a2cb4c51c4ad1a424ee3b4593d424ce8f441
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 19:06:40 +0100

    Add files via upload

---
+++ pgcd_différence.py
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+
+
+# Propriétés mathématiques utilisées : 
+# -----------------------------------------------
+#
+#        si a > b, alors pgcd(a, b) = pgcd(a-b, b)
+#         si a < b, alors pgcd(a, b) = pgcd(a, b-a) 
+#
+# Si l’on veut ppcm(a, b) → utilisez ppcm(a, b) = a*b/pgcd(a, b). 
+
+
+def pgcd_différence (a,b):
+  while (a*b != 0): # ou (a*b > 0)
+    if (a > b):
+      a = a - b
+    else:
+      b = b - a
+if (a == 0):
+  print(“Leur pgcd est  : “,  b)
+else:
+  print(“Leur pgcd est  : ”,  a)
+
+
+a = int(input(“Entrez le premier nombre du pgcd  :  “))
+b = int(input(“Entrez le second nombre du pgcd  :  “))
+pgcd_différence (a, b)
+
+
+----------------------------------------------------------------------- Résultat --------------------------------------------------------------------------
+ scrot.png
